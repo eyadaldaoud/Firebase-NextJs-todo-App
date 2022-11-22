36b21ab4c2318992018256feb5c2dbd60219c1de
UGoingNoWhereBoy <103299832+UGoingNoWhereBoy@users.noreply.github.com>
2022-11-22 15:50:07 -0800

    Update firebase-config.js

--- pages/api/auth/firebase-config.js
+++ pages/api/auth/firebase-config.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore'
-
+/// these keys are useless as the firebase app linked to them is deleted 
+/// you need to add your own keys
 const firebaseConfig = {
   apiKey: "AIzaSyDv5mdAfti6pLzr2XJhnI2lgM1U6kbLmLk",
   authDomain: "myapp-4ce75.firebaseapp.com",
