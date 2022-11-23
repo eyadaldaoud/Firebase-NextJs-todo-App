ffb668e3f2492b3911bd08d9226ae7b318b56d8a
ugoingnowhereboy <ugnw20@gmail.com>
2022-11-22 17:17:53 -0700

    updates for live demo

--- pages/api/auth/firebase-config.js
+++ pages/api/auth/firebase-config.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore'
-/// these keys are useless as the firebase app linked to them is deleted 
+
+/// This is a demo keys and will reach max quota at some point
 /// you need to add your own keys
 const firebaseConfig = {
-  apiKey: process.env.API,
-  authDomain: process.env.DOMAIN,
+  apiKey: "AIzaSyD0bfjzN3pE4jG_wbP1Pdyx94Z_mvrFWy0",
+  authDomain: "myapp-516c7.firebaseapp.com",
   projectId: "myapp-516c7",
   storageBucket: "myapp-516c7.appspot.com",
   messagingSenderId: "611469161616",
-  appId: process.env.APP,
+  appId: "1:611469161616:web:e54fdb68c7e28959c91415"
 };
 
 // Initialize Firebase
